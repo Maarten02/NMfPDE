ac5e9fc3b1dfcc9352a948e1a0f64711c8e14322
Maarten02 <m.e.krikke@student.tudelft.nl>
2021-09-14 09:08:15 +0200

    Plotted the source fcn's and exact soln's properly

--- assignment-2.py
+++ assignment-2.py
@@ -9,19 +9,39 @@ xgrid = np.linspace(-1,1,n+1)
 
 #---------- Source functions -------
 def func1(x):
-    return x**3-x**2+x-3
+    return -6*x+2
 
 def func2(x):
-    return x**4+x**3-x**2-5
+    return -12*x**2-6*x+2
 
 f1 = func1(xgrid)
 f2 = func2(xgrid)
+#--------------- Exact solutions -------
+def u1ex(x):
+    return x**3-x**2+x-3
+
+def u2ex(x):
+    return x**4+x**3-x**2+x-4
+
+u1 = u1ex(xgrid)
+u2 = u2ex(xgrid)
 
 #------- Plot the source functions ------
 plt.figure()
-plt.plot(xgrid, f1, 'b')
-plt.plot(xgrid, f2, 'r')
+plt.plot(xgrid, f1, 'b', label="f1")
+plt.plot(xgrid, f2, 'r', label="f2")
 plt.legend()
 plt.title(r"$f_1$ and $f_2$")
-plt.xlabel(r"$y$")
-plt.ylabel(r"$x$")
+plt.xlabel(r"$x$")
+plt.ylabel(r"$y$")
+plt.show()
+
+#----------- Plot the exact solutions ----
+plt.figure()
+plt.plot(xgrid, u1, 'b', label="u1")
+plt.plot(xgrid, u2, 'r', label="u2")
+plt.legend()
+plt.title(r"$u_1^{ex}$ and $u_2^{ex}$")
+plt.xlabel(r"$x$")
+plt.ylabel(r"$y$")
+plt.show()
