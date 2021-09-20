1cc0cacfc6e8b8c9c8f9289fbf79627082d5c0f2
Maarten02 <m.e.krikke@student.tudelft.nl>
2021-09-20 15:33:45 +0200

    Final code

--- assignment-2.py
+++ assignment-2.py
@@ -2,46 +2,82 @@
 # Maarten Krikke (c) 2021
 import numpy as np
 import matplotlib.pyplot as plt
+import math
 
-#---------- Construct grid ---------
-n = 5
-xgrid = np.linspace(-1,1,n+1)
-
-#---------- Source functions -------
 def func1(x):
     return -6*x+2
 
 def func2(x):
     return -12*x**2-6*x+2
 
-f1 = func1(xgrid)
-f2 = func2(xgrid)
-#--------------- Exact solutions -------
 def u1ex(x):
     return x**3-x**2+x-3
 
 def u2ex(x):
     return x**4+x**3-x**2+x-4
 
-u1 = u1ex(xgrid)
-u2 = u2ex(xgrid)
+def get_system_matrix(n,h):
+    off_diag = np.ones((n-2))*-1
+    diag = np.ones((n-1))*2
+    A_1 = np.diag(off_diag, k=-1)
+    A_2 = np.diag(diag)
+    A_3 = np.diag(off_diag, k=1)
+    return (A_1 + A_2 + A_3)/h**2
 
-#------- Plot the source functions ------
-plt.figure()
-plt.plot(xgrid, f1, 'b', label="f1")
-plt.plot(xgrid, f2, 'r', label="f2")
-plt.legend()
-plt.title(r"$f_1$ and $f_2$")
-plt.xlabel(r"$x$")
-plt.ylabel(r"$y$")
-plt.show()
+def rmse(u_num, u_ex):
+    n = len(u_ex)
+    end = n-1
+    return np.sqrt(np.sum(np.square((u_num - u_ex[1:end])))/(end-1))
 
-#----------- Plot the exact solutions ----
+
+def solution_rmse(n):
+    h=2/n
+    xgrid = np.linspace(-1,1,n+1)
+
+    f1 = func1(xgrid)
+    f2 = func2(xgrid)
+
+    u1ex_xg = u1ex(xgrid)
+    u2ex_xg = u2ex(xgrid)
+
+    A = get_system_matrix(n,h)
+
+    ylb = -6
+    yrb = -2
+
+    f1rhs = f1[1:n]
+    f2rhs = f2[1:n]
+    f1rhs[0] += ylb/h**2
+    f1rhs[n-2] += yrb/h**2
+    f2rhs[0]  += ylb/h**2
+    f2rhs[n-2] += yrb/h**2
+
+    u1_inner = np.linalg.solve(A, f1rhs)
+    u2_inner = np.linalg.solve(A, f2rhs)
+    u1 = np.concatenate([[ylb], u1_inner , [yrb]])
+    u2 = np.concatenate([[ylb], u2_inner , [yrb]])
+    # print(rmse(u1_inner, u1ex_xg), rmse(u2_inner, u2ex_xg))
+    error1 = rmse(u1_inner, u1ex_xg)
+    error2 = rmse(u2_inner, u2ex_xg)
+    # print(error1, error2)
+    print(n)
+    return error1,error2
+
+
+n_range = np.arange(3,1501)
+
+s1_rmse = []
+s2_rmse = []
+for n in n_range:
+    rmse1, rmse2 = solution_rmse(n)
+    s1_rmse.append(rmse1)
+    s2_rmse.append(rmse2)
+
+s1_rmse = np.array(s1_rmse)
+s2_rmse = np.array(s2_rmse)
+
 plt.figure()
-plt.plot(xgrid, u1, 'b', label="u1")
-plt.plot(xgrid, u2, 'r', label="u2")
+plt.loglog(n_range, s1_rmse, 'b', label="$RMSE u_1$")
+plt.loglog(n_range, s2_rmse, 'r', label="$RMSE u_2$")
 plt.legend()
-plt.title(r"$u_1^{ex}$ and $u_2^{ex}$")
-plt.xlabel(r"$x$")
-plt.ylabel(r"$y$")
 plt.show()
