766804eae792b0c31398969199fddfcbe47c46e0
Maarten02 <m.e.krikke@student.tudelft.nl>
2021-10-09 14:20:29 +0200

    ass 5 started

--- assignment-4.py
+++ assignment-4.py
@@ -5,8 +5,8 @@ import scipy.sparse.linalg as la
 
 Lx = 10  # length in x direction
 Ly = 5  # length in y direction
-Nx = 200  # number of intervals in x-direction
-Ny = 100  # number of intervals in y-direction
+Nx = 10  # number of intervals in x-direction
+Ny = 10  # number of intervals in y-direction
 dx = Lx/Nx  # grid step in x-direction
 dy = Ly/Ny  # grid step in y-direction
 
@@ -16,7 +16,7 @@ dy = Ly/Ny  # grid step in y-direction
 #
 
 def sourceF(x,y,alpha=40):
-
+    print("x,y ", x, y)
     f = 0
     for j in range(1,5):
         for i in range(1,10):
@@ -88,26 +88,29 @@ def create2DLFVM(x,y,coeffFun):
 # create grid, source fcn values
 x,y = np.mgrid[dx:Lx:dx, dy:Ly:dy]
 fvec = createF(x,y,sourceF)
-
+print(fvec)
 #--------------------- Plot source fcn and coefficient fcn ------------------------------
-# kvec = createF(x,y,coeffK)
-# reshape fvec and kvec for plotting
-# size = (Ny-1, Nx-1)
-# fvec_reshaped = np.reshape(fvec, size)
-# kvec_reshaped = np.reshape(kvec,size)
-# plt.figure(1)
-# plt.subplot(2,1,1)
-# plt.imshow(fvec_reshaped, origin="lower", extent=((x[0, 0], x[-1, -1], y[0, 0], y[-1, -1])))
-# plt.colorbar(orientation='vertical')
-# plt.subplot(2,1,2)
-# plt.imshow(kvec_reshaped, origin="lower", extent=((x[0, 0], x[-1, -1], y[0, 0], y[-1, -1])))
-# plt.colorbar(orientation='vertical')
-# plt.show()
+kvec = createF(x,y,coeffK)
+print(kvec)
+#reshape fvec and kvec for plotting
+size = (Ny-1, Nx-1)
+fvec_reshaped = np.reshape(fvec, size)
+kvec_reshaped = np.reshape(kvec,size)
+plt.figure(1)
+plt.subplot(2,1,1)
+plt.imshow(fvec_reshaped, origin="lower", extent=((x[0, 0], x[-1, -1], y[0, 0], y[-1, -1])))
+plt.colorbar(orientation='vertical')
+plt.subplot(2,1,2)
+plt.imshow(kvec_reshaped, origin="lower", extent=((x[0, 0], x[-1, -1], y[0, 0], y[-1, -1])))
+plt.colorbar(orientation='vertical')
+plt.show()
 
 #---------------------------------------------------------------------------------------
 
-A = create2DLFVM(x, y, coeffK1)
-
+A = create2DLFVM(x, y, coeffK)
+print(A.toarray())
+print()
+print(A.sum())
 #----------- Verifying system matrix --------------------------
 # print(A.toarray())
 # plt.spy(A, marker='o', markersize=10, color='g')
@@ -118,6 +121,7 @@ uvec = la.spsolve(A, fvec)
 u = np.reshape(uvec,(Ny-1,-1))
 f = np.reshape(fvec, (Ny-1, Nx-1))
 print(u.shape)
+print(u)
 plt.figure(2)
 plt.imshow(u, origin="lower", extent=((x[0, 0], x[-1, -1], y[0, 0], y[-1, -1])))
 #plt.imshow(f, origin="lower", extent=((x[0, 0], x[-1, -1], y[0, 0], y[-1, -1])))
